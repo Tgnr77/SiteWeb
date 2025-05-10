27215819553b8c2abf074a70eee5e2c3a876038e
popolpaulo <you@example.com>
2025-05-10 16:14:55 +0200

    test

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php'; // Inclure le fichier de connexion
-
-try {
-    echo "Connexion à la base de données réussie !";
-} catch (Exception $e) {
-    echo "Erreur : " . $e->getMessage();
-}
-?>
